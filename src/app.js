0c9757530930e3a55a60498652cb15b0792325a9
Nitin Yadav <108917474+SoftwareDeveloperYadavJi@users.noreply.github.com>
2024-12-14 17:00:45 +0530

    Configiure express-rate-limiteder for perventing brude force attack

--- src/app.js
+++ src/app.js
@@ -4,14 +4,28 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const cors = require('cors');
-
+const rateLimit = require('express-rate-limit');
+dotenv.config();
 const app = express();
 
+
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after an hour',
+    standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+    statusCode: 429, // the status code to return on exceeding the limit
+});
+
+
+// All middleware 
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-dotenv.config();
+
 connectDB();
 
 app.use('/api/auth', authRoutes);
